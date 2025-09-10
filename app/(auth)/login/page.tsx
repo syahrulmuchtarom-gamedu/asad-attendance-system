78703b59ed98e72bd0ab79c4a7a8785371cae730
syahrulmuchtarom-gamedu <syahrul.muchtarom@gmail.com>
2025-09-10 17:16:19 +0700

    Add files via upload

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,11 @@
 import { LoginForm } from '@/components/forms/login-form'
 
 export default function LoginPage() {
-  return <LoginForm />
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800 p-4">
+      <div className="w-full max-w-md">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
