39ed8eadcac1f4bf101f2bc46bdab87cdb5ae40c
syahrulmuchtarom-gamedu <syahrul.muchtarom@gmail.com>
2025-10-09 01:17:49 +0700

    Add files via upload

--- app/api/laporan-dki/route.ts
+++ app/api/laporan-dki/route.ts
@@ -2,8 +2,6 @@ import { NextRequest, NextResponse } from 'next/server'
 import { createAdminClient } from '@/lib/supabase/server'
 import { cookies } from 'next/headers'
 
-export const dynamic = 'force-dynamic'
-
 export async function GET(request: NextRequest) {
   try {
     const cookieStore = cookies()
