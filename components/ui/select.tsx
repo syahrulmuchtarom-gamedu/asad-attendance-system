a999c695c96ae4062a9b0c2fc06babfab7ea2d01
syahrulmuchtarom-gamedu <syahrul.muchtarom@gmail.com>
2026-01-15 13:24:56 +0700

    Update select.tsx

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -70,8 +70,7 @@ SelectScrollDownButton.displayName =
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
-  <SelectPrimitive.Portal>
+>(({ className, children, position = "item-aligned", ...props }, ref) => (  <SelectPrimitive.Portal>
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
